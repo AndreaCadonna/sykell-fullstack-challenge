c6882a7397a5084fd5c68f48aff86a1f92588090
Andrea Cadonna <andrea.cadonna.dev@gmail.com>
2025-07-07 20:32:07 +0200

    added shadcn ui components

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,21 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
     port: 3000,
     watch: {
       usePolling: true,
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   test: {
